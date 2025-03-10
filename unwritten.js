61a943493daa2218ba9c0474e48de11b79370996
Raven Cunanan <63638637+RavenCunanan@users.noreply.github.com>
2025-03-10 16:57:03 -0700

    Create unwritten.js

---
+++ unwritten.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const message = 'Someday, you\'ll look back on this moment and cringe a lot...but that\'s ok!'
+ 
+fs.writeFile('seventh-grade.txt', message, (err) => {
+  if (err) {
+    console.error('Error rewriting diary:', err);
+  } else {
+    console.log('Diary rewritten successfully!');
+  }
+});
